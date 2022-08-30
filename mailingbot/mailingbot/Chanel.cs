610871a6f62370aa6e2878a8547b8d3d19adf9a9
mojnooymeret <95657364+mojnooymeret@users.noreply.github.com>
2022-08-30 23:06:38 +0500

    Add files via upload

---
+++ mailingbot/mailingbot/Chanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mailingbot
+{
+   internal class Chanel
+   {
+      public int id { get; set; }
+      public string id_chanel { get; set; }
+      public string name { get; set; }
+      public string id_user { get; set; }
+      public string chanels { get; set; }
+      public Chanel(int id, string id_chanel, string name, string id_user)
+      {
+         this.id = id;
+         this.id_chanel = id_chanel;
+         this.name = name;
+         this.id_user = id_user;
+      }
+   }
+}
---
+++ mailingbot/mailingbot/User.cs
@@ -0,0 +1,15 @@
+﻿namespace mailingbot
+{
+   internal class User
+   {
+      public string id { get; set; }
+      public string first_name { get; set; }
+      public string id_chanel { get; set; }
+      public User(string id, string first_name, string id_chanel)
+      {
+         this.id = id;
+         this.first_name = first_name;
+         this.id_chanel = id_chanel;
+      }
+   }
+}
